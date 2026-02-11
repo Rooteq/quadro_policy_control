1a31f76a80c1616a1043a4dcbf88956ce304ea4e
Rooteq <szymerut@gmail.com>
2026-02-11 21:57:31 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'quadro_policy_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rooteq',
+    maintainer_email='szymerut@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'quadro_controller = quadro_policy_control.quadro_controller:main'
+        ],
+    },
+)
